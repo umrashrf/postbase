329a8c8c7b2031e68f3d05f4066a96992bf93952
Umair Ashraf <umrashrf@gmail.com>
2025-11-05 20:33:10 -0700

    Securing backend with https

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,13 @@
-import './env.js'; // for loading .env file
+import './env.js';
+import fs from 'node:fs';
+import https from 'node:https';
+import { app } from './server.js';
 
-import { app } from "./app";
+const httpsOptions = {
+    key: fs.readFileSync(process.env.LETSENCRYPT_KEY),
+    cert: fs.readFileSync(process.env.LETSENCRYPT_CERT)
+};
 
-const PORT = process.env.POSTBASE_BACKEND_HTTP_PORT || 8081;
-app.listen(PORT, () => console.log(`Postbase backend listening on ${PORT}`));
+const HTTPS_PORT = process.env.POSTBASE_BACKEND_HTTPS_PORT || 4431;
+
+https.createServer(httpsOptions, app).listen(HTTPS_PORT, () => console.log(`Postbase backend listening on https://0.0.0.0:${HTTPS_PORT}`));
