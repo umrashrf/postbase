9195d561237e494634458d71c01b3aea52c6f280
Umair Ashraf <umrashrf@gmail.com>
2025-11-09 23:12:22 -0700

    Enable reset password

--- backend/auth.js
+++ backend/auth.js
@@ -23,6 +23,17 @@ export const auth = betterAuth({
     }),
     emailAndPassword: {
         enabled: true,
+        sendResetPassword: async ({ user, url, token }, request) => {
+            // await sendEmail({
+            //     to: user.email,
+            //     subject: "Reset your password",
+            //     text: `Click the link to reset your password: ${url}`,
+            // });
+        },
+        // onPasswordReset: async ({ user }, request) => {
+        //     // your logic here
+        //     console.log(`Password for user ${user.email} has been reset.`);
+        // },
     },
     // emailVerification: {
     //     sendVerificationEmail: async ({ user, url }) => {
