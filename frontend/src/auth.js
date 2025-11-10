83fb539802525516358d5b7f10682f0179e6d3c1
Umair Ashraf <umrashrf@gmail.com>
2025-11-09 21:12:32 -0700

    Fix frontend auth code

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,7 +1,10 @@
+import { createAuthClient as createBetterAuthClient } from 'better-auth/client';
 import { createAuthClient } from '../lib/postbase/auth';
 
-export const authClient = createAuthClient({
+export const authClient = createBetterAuthClient({
     baseURL: import.meta.env.VITE_API_BASE + '/auth', // Specify if on a different domain/path,
 });
+
+export const auth = createAuthClient(authClient);
 
 export const { signUp, signIn, signOut, getSession } = authClient;
