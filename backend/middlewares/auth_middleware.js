69b8dabec241ff6e164fba5cffe1cbb0570f2f81
Umair Ashraf <umrashrf@gmail.com>
2025-11-14 13:34:11 -0700

    Add sample use of auth middleware in backend

---
+++ backend/middlewares/auth_middleware.js
@@ -0,0 +1,10 @@
+// server/middleware/auth.js
+import { createPool } from '../../lib/postbase/db.js';
+import { authMiddleware } from '../../lib/postbase/middlewares/auth.js';
+
+// Initialize DB pool using env variables
+const pool = createPool({
+    connectionString: process.env.DATABASE_URL
+});
+
+export const authenticate = await authMiddleware(pool);
