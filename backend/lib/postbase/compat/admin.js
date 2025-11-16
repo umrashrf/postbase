c0192c41907cea8405974208efd3f2dd255cc30b
Umair Ashraf <umrashrf@gmail.com>
2025-11-15 17:35:59 -0700

    Add firebase admin compat file

---
+++ backend/lib/postbase/compat/admin.js
@@ -0,0 +1,30 @@
+// Backward compatability with admin firebase
+
+import { makePostbaseAdminClient } from '../adminClient.js';
+
+export function createAdminClient(
+    {
+        authClient = null,
+        pool = null,
+    } = {}) {
+    return {
+        auth: () => {
+            if (!authClient) throw new Error("authClient is missing. please check postbase/backend/auth.js");
+            return {
+                getUser: async (userId) => {
+                    const { data: user, error } = await authClient.admin.getUser({ userId });
+                    const userObj = {
+                        ...user,
+                        displayName: user.name,
+                    };
+                    return userObj;
+                },
+            };
+        },
+        firestore: () => {
+            if (!pool) throw new Error("pool is missing. please check postbase/backend/app.js or use createPool from backend/lib/postbase/db.js");
+            const db = makePostbaseAdminClient({ pool });
+            return db;
+        },
+    };
+}
