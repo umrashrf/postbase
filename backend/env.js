948fb630dae0e58a0c0d2f7f60cf13b9ef6c4ade
Umair Ashraf <umrashrf@gmail.com>
2025-11-20 13:34:51 -0700

    Load .env from backend directory regardless of where the script is called from

--- backend/env.js
+++ backend/env.js
@@ -1,2 +1,12 @@
 import dotenv from 'dotenv';
-dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dotenvPath = __dirname + '/.env';
+console.log('Loading .env from:', __dirname + '/.env');
+dotenv.config({
+    path: dotenvPath,
+});
