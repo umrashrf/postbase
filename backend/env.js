3b7ac54db47c1f5c1bc6174063c1f232369e1c89
Umair Ashraf <umrashrf@gmail.com>
2025-12-23 19:07:57 -0700

    Minor improvement

--- backend/env.js
+++ backend/env.js
@@ -5,8 +5,8 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dotenvPath = __dirname + '/.env';
-console.log('Loading .env from:', __dirname + '/.env');
+const dotenvPath = path.join(__dirname, '.env');
+console.log('Loading .env from:', dotenvPath);
 dotenv.config({
     path: dotenvPath,
 });
