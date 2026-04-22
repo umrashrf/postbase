8873e17b0cef223758f6598a50659a5319c99b5e
Umair Ashraf <umrashrf@gmail.com>
2026-04-21 18:17:36 -0600

    Fix baseUrl in authClient

--- backend/admin/auth.js
+++ backend/admin/auth.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/client";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.API_BASE,
+    baseURL: process.env.API_BASE + '/auth',
     plugins: [
         adminClient()
     ]
