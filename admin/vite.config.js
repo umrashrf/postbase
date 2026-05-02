dd59bf70f7ff503da357a557a54995ac2d75ace4
Umair Ashraf <umrashrf@gmail.com>
2026-05-02 00:11:41 -0600

    Set default port different from frontend

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,6 +3,9 @@ import preact from '@preact/preset-vite';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    server: {
+        port: 5174,
+    },
     plugins: [
         preact({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
         tailwindcss(),
