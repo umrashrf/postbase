b0054a54bb20b45bf3f14c32d93dc52061bba9a9
Umair Ashraf <umrashrf@gmail.com>
2026-06-13 19:06:50 -0400

    Fix api local main entry

--- backend/local.js
+++ backend/local.js
@@ -1,6 +1,14 @@
 import './env.js'; // for loading .env file
+import http from 'node:http';
+import { setupWebsocket } from './lib/postbase/websocket.js';
+import { app } from "./src/app.js";
 
-import { app } from "./app.js";
+const HTTPS_PORT = process.env.POSTBASE_BACKEND_HTTP_PORT || 8081;
 
-const PORT = process.env.POSTBASE_BACKEND_HTTP_PORT || 8081;
-app.listen(PORT, () => console.log(`Postbase backend listening on http://0.0.0.0:${PORT}`));
+const server = http.createServer({}, app);
+
+setupWebsocket({ server });
+
+server.listen(HTTPS_PORT,
+    () => console.log(`Postbase backend listening on https://0.0.0.0:${HTTPS_PORT}`));
+
