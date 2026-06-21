73f894c049679f019537254d4855ad1d3dfdd904
Umair Ashraf <umrashrf@gmail.com>
2026-06-21 12:31:17 -0400

    Add db hint

--- backend/lib/postbase/tests/db.js
+++ backend/lib/postbase/tests/db.js
@@ -5,5 +5,6 @@ export const pool = new Pool({
 });
 
 export async function resetDb() {
+    // export TEST_DB_URL="postgresql://postgres:yoursecretpassword@127.0.0.1:5432/postbase"
     await pool.query(`TRUNCATE rtdb_nodes`);
 }
