6dbaaa76a21a375959c9e6fa243a3db0cfbe921b
Umair Ashraf <umrashrf@gmail.com>
2026-09-09 14:53:07 -0400

    Use localhost for backend and enable node clustering

--- backend/local.js
+++ backend/local.js
@@ -1,14 +1,23 @@
 import './env.js'; // for loading .env file
+import os from 'node:os';
 import http from 'node:http';
+import cluster from 'node:cluster';
 import { setupWebsocket } from './lib/postbase/websocket.js';
 import { app } from "./app.js";
 
-const HTTPS_PORT = process.env.POSTBASE_BACKEND_HTTP_PORT || 8081;
+const HTTPS_PORT = process.env.POSTBASE_BACKEND_HTTP_PORT || "8081";
 
-const server = http.createServer({}, app);
+if (cluster.isPrimary) {
+    const workers = os.availableParallelism();
 
-setupWebsocket({ server });
+    for (let i = 0; i < workers; i++) {
+        cluster.fork();
+    }
+} else {
+    const server = http.createServer({}, app);
 
-server.listen(HTTPS_PORT,
-    () => console.log(`Postbase backend listening on http://0.0.0.0:${HTTPS_PORT}`));
+    setupWebsocket({ server });
 
+    server.listen(parseInt(HTTPS_PORT), "127.0.0.1",
+        () => console.log(`Postbase backend listening on http://127.0.0.1:${HTTPS_PORT}`));
+}
